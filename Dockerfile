9b082ae9e0863aa87197573ce2218102637c426a
Daniil Rakityansky <danch@MBPro.local>
2022-07-17 11:33:46 +0300

    docker server

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
-FROM nikolaik/python-nodejs:python3.10-nodejs16-slim
+FROM python:3.10-slim
 COPY . /app
-WORKDIR /app/frontend
-RUN yarn install && yarn build
 WORKDIR /app
 RUN pip install -r requirements.txt
-RUN chmod +x ./upgrade_migrations.sh
-CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "3000"]
--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
-from fastapi.staticfiles import StaticFiles
 from routers.building import router as building_router
 from routers.offer import router as offer_router
 
@@ -20,16 +19,3 @@ app.include_router(building_router, prefix="/building")
 app.include_router(offer_router, prefix="/offer")
 app.add_middleware(CORSMiddleware, allow_origins=[
                    "*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"],)
-
-
-class SPAStaticFiles(StaticFiles):
-    async def get_response(self, path: str, scope):
-        response = await super().get_response(path, scope)
-        if response.status_code == 404:
-            response = await super().get_response(".", scope)
-        return response
-
-
-app.mount(
-    "/", SPAStaticFiles(directory="./frontend/dist", html=True), name="mainapp"
-)
